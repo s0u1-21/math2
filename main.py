c40f071832efa0ac069b6b0f4beabd3a14d1683d
tolenfra000 <tolenfra000@warren.k12.in.us>
2024-09-17 10:05:07 -0400

    fixed

---
+++ main.py
@@ -0,0 +1,15 @@
+miles = 5 #pretend this came from user input
+minutes = 45 #pretend this came from user input
+hours = minutes / 60
+
+mph = miles / hours
+
+print("How many miles did you run?", miles)
+#INSTRUCTION: write a print statement
+# that says "How many minutes did
+# it take you?" "  use the minutes variable
+print("How many minutes did it take you?", minutes)
+#INSTRUCTION write a print statement
+# that says "Speed in mph:"
+# use the mph variable
+print("Speed in mph:", mph)
